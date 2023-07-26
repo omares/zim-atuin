a25deadfb25a80d6aa4eb33f08e86f0a087df897
Ota Mares <1375307+omares@users.noreply.github.com>
2023-07-26 14:32:08 +0200

    Improve readability, cleanup variables, ignore generated file

--- init.zsh
+++ init.zsh
@@ -1,5 +1,9 @@
-if (( $+commands[atuin] )); then
-    _PLUGIN_DIR="${0:A:h}"
-    $commands[atuin] init zsh >| ${_PLUGIN_DIR}/_atuin.zsh
-    source ${_PLUGIN_DIR}/_atuin.zsh
+if (( ! $+commands[atuin] )); then
+    return 1
 fi
+
+local _plugin_dir="${0:A:h}"
+$commands[atuin] init zsh >| ${_plugin_dir}/_atuin.zsh
+source ${_plugin_dir}/_atuin.zsh
+
+unset _plugin_dir
